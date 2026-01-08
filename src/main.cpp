dad5d3c5d28f0550abf8fdf64915120bd707ac69
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-01-09 00:42:59 +0500

    modified for testing json files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "../include/taskHandler.h"
 #include "../include/file.h"
+#include "../include/json.h"
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -9,8 +10,20 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    JsonObject o1;
+    string what = "hassan";
+    o1.appendKeyValue("name", static_cast<void*>(&what));
+    cout << *static_cast<string*>(o1.getValue("name")) << endl;
+
     File* file = new File("data/tasks.json");
-    file->readFile();
+    string* data = file->readFile();
+
+    for (int i = 0; data[i] != "\0"; i++) {
+        vector<JsonObject*> objects = JSON::instance()->readLine(data[i]);
+        for (int j = 0; j < objects.size(); j++) {
+            objects[j]->print();
+        }
+    }
 
     // if (argv[1] == "add") {
     //     TaskHandler::instance()->addTask(argv[2]);
