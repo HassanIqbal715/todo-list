2b2298bf5d7740135dcc42ace34ed67e61aecf02
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-01-08 22:12:37 +0500

    testing creating and reading file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include "../include/taskHandler.h"
+#include "../include/file.h"
 using namespace std;
 
-int main() {
-    cout << "Testing stuff\n";
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        cerr << "Unexpected: File path not provided!\n";
+        return 1;
+    }
+
+    File* file = new File("data/tasks.json");
+    file->readFile();
+
+    // if (argv[1] == "add") {
+    //     TaskHandler::instance()->addTask(argv[2]);
+    // }
+    // else if (argv[1] == "update") {
+    //     TaskHandler::instance()->updateTask();
+    // }
+    // else if (argv[1] == "delete") {
+    //     TaskHandler::instance()->deleteTask();
+    // }
+    // else if (argv[1] = "list") {
+    //     TaskHandler::instance()->listTasks();
+    // }
 }
