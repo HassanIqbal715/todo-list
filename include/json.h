851e1cf3bdec18c8c8de387719da8ea6945cc39a
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-01-09 00:44:18 +0500

    Added support for reading JSON files. - An unordered map for storing key value pairs. - line by line feeding of data and tokenizing it. - extracted pairs are returned as a custom objects vector.

---
+++ include/json.h
@@ -0,0 +1,40 @@
+#pragma once
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+class JsonObject {
+private:
+    unordered_map<string, void*> keyValuePairs;
+    bool checkKeyExist(string key);
+
+public:
+    JsonObject();
+    void appendKeyValue(string key, void* value);
+    void print();
+    void* getValue(string key);
+    ~JsonObject();
+};
+
+enum TokenType {KEY, VALUE};
+
+struct Token {
+    TokenType type;
+    string value;
+};
+
+class JSON {
+private:
+    vector<Token*> tokens;
+    vector<JsonObject*> objectsBuffer; // Temporarily stores objects here
+    static JSON* Instance;
+    ~JSON();
+
+public:
+    static JSON* instance();
+    void storeToken(string value, TokenType type);
+    void tokenize(string line);
+    void extractKeyValue();
+    vector<JsonObject*> readLine(string line);
+};
---
+++ src/json.cpp
@@ -0,0 +1,145 @@
+#include"../include/json.h"
+
+JsonObject::JsonObject() {}
+
+bool JsonObject::checkKeyExist(string key) {
+    auto it = keyValuePairs.find(key);
+    return it != keyValuePairs.end();
+}
+
+void JsonObject::appendKeyValue(string key, void* value) {
+    if (!checkKeyExist(key))
+        keyValuePairs.insert(pair(key, value));
+}
+
+void JsonObject::print() {
+    if (keyValuePairs.empty()) return;
+    cout << "{\n";
+    for (auto& it : keyValuePairs) {
+        cout << '\t' << it.first << ": " << 
+            *static_cast<string*>(it.second) << '\n';
+    }
+    cout << "}\n";
+}
+
+void* JsonObject::getValue(string key) {
+    auto it = keyValuePairs.find(key);
+    if (it != keyValuePairs.end())
+        return it->second;
+    return nullptr;
+}
+
+JsonObject::~JsonObject() {}
+
+JSON* JSON::Instance = nullptr;
+
+JSON* JSON::instance() {
+    if (Instance == nullptr) {
+        Instance = new JSON;
+    }
+    return Instance;
+}
+
+void JSON::storeToken(string value, TokenType type) {
+    tokens.push_back(new Token{type, value});
+}
+
+void JSON::tokenize(string line) {
+    bool hasStarted = false;
+    bool hasEnded = false;
+    bool isReading = false;
+    bool isString = false;
+    char buffer[500];
+    int bufferIndex = 0;
+
+    for (int i = 0; i < line.length(); i++) {
+        switch(line[i]) {
+            case '{':
+                buffer[bufferIndex] = '\0';
+                bufferIndex = 0;
+                hasStarted = true; 
+                hasEnded = false;
+                isReading = true;
+                break;
+
+            case '}': 
+                buffer[bufferIndex] = '\0';
+                bufferIndex = 0;
+                storeToken(buffer, TokenType::VALUE);
+                extractKeyValue();
+                hasEnded = true; 
+                hasStarted = false;
+                isReading = false;
+                break;
+
+            case ':': 
+                if (isString) {
+                    buffer[bufferIndex++] = line[i];
+                    break;
+                }
+
+                buffer[bufferIndex] = '\0';
+                bufferIndex = 0;
+                storeToken(buffer, TokenType::KEY);
+                isReading = false;
+                break;
+
+            case ',':
+                if (isString) {
+                    buffer[bufferIndex++] = line[i];
+                    break;
+                }
+
+                buffer[bufferIndex] = '\0';
+                bufferIndex = 0;
+                if (!hasEnded)
+                    storeToken(buffer, TokenType::VALUE);
+                if (hasEnded == true && hasStarted == false) {
+                    hasStarted = false;
+                    hasEnded = false;
+                }
+                isReading = false;
+                break;
+
+            case '"':
+                isString = !isString;
+            case '\t':
+            case '\r':
+            case '\n':
+                break;
+
+            case ' ':
+                if (isReading)
+                    buffer[bufferIndex++] = line[i];
+                break;
+
+            default:
+                isReading = true;
+                buffer[bufferIndex++] = line[i];
+                break;
+        }
+    }
+}
+
+void JSON::extractKeyValue() {
+    JsonObject* obj = new JsonObject;
+    for (int i = 0; i < tokens.size(); i++) {
+        if (tokens[i]->type == TokenType::KEY) {
+            obj->appendKeyValue(tokens[i]->value, 
+                static_cast<void*>(&tokens[i+1]->value));
+        }
+    }
+    objectsBuffer.push_back(obj);
+    tokens.clear();
+}
+
+vector<JsonObject*> JSON::readLine(string line) {
+    tokens.clear();
+    objectsBuffer.clear();
+    tokenize(line);
+    return objectsBuffer;
+}
+
+JSON::~JSON() {
+
+}
