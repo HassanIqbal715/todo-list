5bbdcf9ed688988efacef3a7f51707effb13fb25
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-02-10 13:51:27 +0500

    Added function. Task update

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     if (argc <= 1) {
-        cerr << "Unexpected: File path not provided!\n";
+        cerr << "Unexpected: No argument provided!\n";
         return 1;
     }
 
@@ -31,14 +31,26 @@ int main(int argc, char* argv[]) {
 
     string arg1 = argv[1];
     if (arg1.compare("add") == 0) {
-        cout << "what";
         TaskHandler::instance()->addTask(tasks, argv[2]);
         TaskHandler::instance()->writeTasks(tasks, file);
     }
+    else if (arg1.compare("update") == 0) {
+        if (argv[2] == NULL || argv[2] == "" || 
+            argv[3] == NULL || argv[3] == "")
+            return 1;
 
-    // else if (argv[1] == "update") {
-    //     TaskHandler::instance()->updateTask();
-    // }
+        int idArg = stoi(argv[2]);
+        TaskStatus statusArg = stringToTaskStatus(argv[3]);
+        
+        if (statusArg == TaskStatus::NA) {
+            cout << "Failed! wrong status argument. pick between: todo,";
+            cout << "\"in progress\" and done\n";
+            return 1;
+        }
+        
+        TaskHandler::instance()->updateTask(tasks, idArg, statusArg);
+        TaskHandler::instance()->writeTasks(tasks, file);
+    }
     else if (arg1.compare("delete") == 0) {
         if (argv[2] == NULL || argv[2] == "")
             return 1;
@@ -49,7 +61,7 @@ int main(int argc, char* argv[]) {
     else if (arg1.compare("list") == 0) {
         TaskHandler::instance()->listTasks(tasks);
     }
-    
+
     delete[] data;
     delete file;
     JSON::instance()->destroyObjectsVector(objects);
